d4fef73c7a025aad4a6a1518bf21f2ebb68ed99b
Miguel Angel Sanchez Martinez <masanmar.s70@gmail.com>
2025-05-09 13:27:27 -0600

    Adición de servicios de multiplicación y división

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,6 +25,16 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2", "ERROR SUBSTRACT"
+        )
+
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
